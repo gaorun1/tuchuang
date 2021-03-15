0af5c5044399703e6781e9a51b6e54bc309642b4
gaorun <568726695@qq.com>
2021-03-15 16:56:56 +0800

    Revert "update by gitPic"
    This reverts commit 60c7be62a43c3256a435c5488f269e5554cf1bec.

--- startTC.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title ¿ªÆôÍ¼´²
-start  D:\development\Java\jdk-11.0.10\bin\java.exe -jar gitPic.jar
-
-
-
-rem D:
-rem cd D:\workspace\github\tuchuang\
-rem start  D:\development\Java\jdk-11.0.10\bin\java.exe -jar gitPic.jar
