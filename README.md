46b5eedd524eb861defc1039072bf76013fc7a8d
gaorun <568726695@qq.com>
2021-03-15 17:03:22 +0800

    添加说明

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tuchuang
-1
+使用github作为图床
+
+本程序为图床
+
+参考项目：https://github.com/zzzzbw/gitPic
---
+++ startTC.bat
@@ -0,0 +1,9 @@
+@echo off
+title ¿ªÆôÍ¼´²
+start  D:\development\Java\jdk-11.0.10\bin\java.exe -jar gitPic.jar
+
+
+
+rem D:
+rem cd D:\workspace\github\tuchuang\
+rem start  D:\development\Java\jdk-11.0.10\bin\java.exe -jar gitPic.jar
